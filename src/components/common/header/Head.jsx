88c9bb2bddc53bdc29134441590b96a39b744485
Vidya Gowda <vidya09.gowda@gmail.com>
2025-08-27 22:07:40 +0530

    Update Head.jsx

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -4,14 +4,7 @@ const Head = () => {
   return (
     <>
       <section className='head'>
-        <div className='container flexSB paddingTB'>
-          <div className='logo'>
-            <img src='../images/logo.png' alt='' />
-          </div>
-          <div className='ad'>
-            <img src='../images/headerb.png' alt='' />
-          </div>
-        </div>
+        
       </section>
     </>
   )
